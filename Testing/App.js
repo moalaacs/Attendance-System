0984c36812ecf6093c85705ccab06cf388fc5d6a
Mohammed Alaa <moalaacs@gmail.com>
2023-01-19 04:25:17 +0200

    Login validation

--- Testing/App.js
+++ Testing/App.js
@@ -1,50 +1,80 @@
 import { Employee } from "../Testing/Emp.js";
 let tbody = document.getElementsByTagName("tbody")[0];
-console.log(tbody);
+
 fetch("http://localhost:3000/employees")
   .then((result) => {
     return result.json();
   })
-  .then((empData) => {
-    console.log(empData);
-    empData.forEach((user) => {
+  .then((employeeData) => {
+    console.log(employeeData);
+    employeeData.forEach((employee) => {
       let tr = document.createElement("tr");
-      let names = document.createElement("td");
-      let date = document.createElement("td");
-      let atend = document.createElement("td");
-      let departeure = document.createElement("td");
-      atend.innerText = user.attend[0].attend;
-      departeure.innerText = user.attend[0].departure;
-      date.innerText = user.attend[0].date;
-      names.innerText = user.name;
+      let firstName = document.createElement("td");
+      let lastName = document.createElement("td");
+      let address = document.createElement("td");
+      let email = document.createElement("td");
+      let dob = document.createElement("td");
+      let role = document.createElement("td");
 
-      tr.appendChild(names);
-      tr.appendChild(date);
-      tr.appendChild(atend);
-      tr.appendChild(departeure);
-      if (Number(user.attend[0].attend) > 9) {
-        tr.style.backgroundColor = "red";
-      } else tr.style.backgroundColor = "green";
+      firstName.innerText = employee.firstname;
+      lastName.innerText = employee.lastname;
+      address.innerText = employee.address;
+      email.innerText = employee.email;
+      dob.innerText = employee.dob;
+      role.innerText = employee.role;
+
+      tr.appendChild(firstName);
+      tr.appendChild(lastName);
+      tr.appendChild(address);
+      tr.appendChild(email);
+      tr.appendChild(dob);
+      tr.appendChild(role);
+
+      // if (Number(user.attend[0].attend) > 9) {
+      //   tr.style.backgroundColor = "red";
+      // } else tr.style.backgroundColor = "green";
       tbody.appendChild(tr);
     });
   });
 
 let e1 = new Employee("hambola", 15, "dep1", "ceo", [
-  { date: "1/1/2022", attend: "9.45", departure: "3" },
+  { date: "1/1/2022", in: "9.45", out: "3" },
 ]);
 
-fetch("http://localhost:3000/employees", {
-  method: "POST",
-  body: JSON.stringify({
-    id: 205,
-    name: e1.Name,
-    attend: e1.Attendence_info,
-  }),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8",
-  },
-})
-  .then((response) => response.json())
+let e = new Employee(
+  "Mikaela",
+  "Nunn",
+  "719 Farragut Trail",
+  "mnunnd@networksolutions.com",
+  "8/4/1976",
+  "employee",
+  [
+    {
+      date: "1/1/2023",
+      in: "9",
+      out: "3",
+    },
+  ]
+);
 
-  // Displaying results to console
-  .then((json) => console.log(json));
+// fetch("http://localhost:3000/employees", {
+//   method: "POST",
+//   body: JSON.stringify({
+//     "id": 205,
+//     "name": e1.Name,
+//     "attend": e1.Attendence_info,
+//     "firstname": e.firstname,
+//     "lastname": e.lastname,
+//     "address": e.address,
+//     "email": e.email,
+//     "dob": e.dob,
+//     "role": e.role,
+//   }),
+//   headers: {
+//     "Content-type": "application/json; charset=UTF-8",
+//   },
+// })
+//   .then((response) => response.json())
+
+//   // Displaying results to console
+//   .then((json) => console.log(json));
---
+++ Views/Authentication/Login/JS/login.js
@@ -0,0 +1,34 @@
+let loginForm = document.querySelector("form");
+let loginBtn = document.querySelector(".login-btn");
+let loginEmail = document.querySelector("#login-email");
+let loginPassword = document.querySelector("#login-password");
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  let validEmail = loginEmail.checkValidity();
+  let validPassword = loginPassword.checkValidity();
+  if (validEmail && validPassword) {
+    checkCredential();
+  }
+})
+
+async function checkCredential() {
+  let response = await fetch(`http://localhost:3000/employees?email=${loginEmail.value}&&password=${loginPassword.value}`);
+  let user = await response.json();
+  if (user.length == 0) {
+    console.log("Wrong email or password");
+  }
+  else {
+    if (user[0].role == "admin") {
+      console.log("Admin");
+    }
+    else if (user[0].role == "security") {
+      console.log("Sec");
+    }
+    else if (user[0].role == "employee") {
+      console.log("Emp");
+    }
+  }
+  console.log(user);
+}
